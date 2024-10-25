598e6d720d734a0a9220e1e6e2b7a07ef9391cb7
ahmed-abdat <ahmedeabdat@gmail.com>
2024-10-25 20:10:05 +0100

    add opengraph

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -12,6 +12,17 @@ interface HomeProps {
 export const metadata: Metadata = {
   title: "SMTD - Pour un Environnement Plus Propre",
   description: "Leader en gestion et valorisation des déchets en Mauritanie. Découvrez nos services de collecte, recyclage et traitement écologique des déchets.",
+  openGraph: {
+    title: "SMTD - Solutions Durables pour la Gestion des Déchets",
+    description: "Explorez comment SMTD transforme la gestion des déchets en Mauritanie pour un avenir plus vert.",
+    images: ['/opengraph-image.jpg'], // Add the path to your home page Open Graph image
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "SMTD - Innovons pour un Environnement Plus Propre",
+    description: "Découvrez l'impact de SMTD sur la gestion des déchets et le développement durable en Mauritanie.",
+    images: ['/twitter-image.jpg'], // Add the path to your home page Twitter card image
+  },
 };
 
 export default function Home({
